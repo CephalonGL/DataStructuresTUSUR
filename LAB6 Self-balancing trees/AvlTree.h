3f29fa005a27c4c73a28c04c8d00bbdfbbe5ab02
Gregorii Lubov <glubov@yandex.ru>
2022-02-13 21:43:50 +0700

    style(AvlTree, AvlTreeNode): XML

--- LAB6 Self-balancing trees/AvlTree.h
+++ LAB6 Self-balancing trees/AvlTree.h
@@ -3,19 +3,74 @@
 #include "AvlTreeNode.h"
 using namespace std;
 
+/// <summary>
+/// AVL-tree the data structure
+/// </summary>
 class AvlTree
 {
 public:
+	/// <summary>
+	/// Inserts new node
+	/// </summary>
+	/// <param name="keyToInsert">Key of new node</param>
+	/// <param name="valueToInsert">Value of new node</param>
 	void Insert(int keyToInsert, string valueToInsert);
+
+	/// <summary>
+	/// Get value by inserted key
+	/// </summary>
+	/// <param name="keyToSearch">Key to search</param>
+	/// <returns>Value</returns>
 	string GetValueByKey(int keyToSearch);
+
+	/// <summary>
+	/// Delete node by inserted key
+	/// </summary>
+	/// <param name="keyToDelete">Key of node to delete</param>
 	void DeleteByKey(int keyToDelete);
+
+	/// <summary>
+	/// Initialize a tree
+	/// </summary>
+	/// <param name="rootKey">Key of root node</param>
+	/// <param name="rootValue">Value of root node</param>
 	AvlTree(int rootKey, string rootValue);
 private:
+	/// <summary>
+	/// Pointer to root node
+	/// </summary>
 	AvlTreeNode _root;
+
+	/// <summary>
+	/// Find node by the key
+	/// </summary>
+	/// <param name="keyToFind">Key to find node by</param>
+	/// <returns>Pointer to node</returns>
 	AvlTreeNode* _FindNodeByKey(int keyToFind);
+
+	/// <summary>
+	/// Balances subtrees of node
+	/// </summary>
+	/// <param name="node">Node to balance which subtrees</param>
 	void _GoBalance(AvlTreeNode* node);
+
+	/// <summary>
+	/// Do small left rotation
+	/// </summary>
 	void _SmallLeftRotation();
+
+	/// <summary>
+	/// Do small right rotation
+	/// </summary>
 	void _SmallRightRotation();
+
+	/// <summary>
+	/// Do big left rotation
+	/// </summary>
 	void _BigLeftRotation();
+
+	/// <summary>
+	/// Do big right rotation
+	/// </summary>
 	void _BigRightRotation();
 };
--- LAB6 Self-balancing trees/AvlTreeNode.h
+++ LAB6 Self-balancing trees/AvlTreeNode.h
@@ -1,14 +1,39 @@
 #pragma once
 #include <string>
-
 using namespace std;
 
+/// <summary>
+/// Node for AVL-tree data structure
+/// </summary>
 struct AvlTreeNode
 {
+	/// <summary>
+	/// Key of node
+	/// </summary>
 	int _key = 0;
+
+	/// <summary>
+	/// Value of node
+	/// </summary>
 	string _value = "";
+
+	/// <summary>
+	/// Pointer to parent node
+	/// </summary>
 	AvlTreeNode* _parent = nullptr;
+
+	/// <summary>
+	/// Pointer to left node
+	/// </summary>
 	AvlTreeNode* _leftSubtree = nullptr;
+
+	/// <summary>
+	/// Pointer to right node
+	/// </summary>
 	AvlTreeNode* _rightSubtree = nullptr;
+
+	/// <summary>
+	/// Height difference between left and right subtrees
+	/// </summary>
 	short int _height = 0;
 };
