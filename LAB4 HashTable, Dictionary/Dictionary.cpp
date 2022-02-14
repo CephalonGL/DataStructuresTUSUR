cf5aabb28d931ea0a4360e893ccc467b289776c5
Gregorii Lubov <glubov@yandex.ru>
2022-02-14 19:10:24 +0700

    feature(Dictionary): добавлена реализация функции DictionaryInsert

--- LAB4 HashTable, Dictionary/Dictionary.cpp
+++ LAB4 HashTable, Dictionary/Dictionary.cpp
@@ -1,11 +1,25 @@
 //TODO:
 #include "Dictionary.h"
+#include "HashTable.h"
 #include <string>
 
 using namespace std;
 
 void Dictionary::DictionaryInsert(string key, string value)
 {
+	HashTableNode* currentNode = &_hashTable->HashTableNodeArray[_hashTable->Hash(key)];
+	while (currentNode)
+	{
+		if (currentNode->Key == key)
+		{
+			throw string("Ошибка: в словаре уже есть элемент с таким ключём");
+		}
+		else
+		{
+			currentNode = currentNode->NextCollision;
+		}
+	}
+	_hashTable->HashTableInsert(key, value);
 }
 
 void Dictionary::Remove(string key)
