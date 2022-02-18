0acba9b4f12bc11993d5aa3d0c8e9e3b79f24561
Gregorii Lubov <glubov@yandex.ru>
2022-02-18 19:55:43 +0700

    style(AvlTree): fix XML

--- LAB6 Self-balancing trees/RbTreeNode.h
+++ LAB6 Self-balancing trees/RbTreeNode.h
@@ -1,5 +1,31 @@
 ﻿#pragma once
+#include <string>
+
+using namespace std;
+
+/// <summary>
+/// 
+/// </summary>
 class RbTreeNode
 {
+private:
+	/// <summary>
+	/// Key of node
+	/// </summary>
+	int _key;
+
+	/// <summary>
+	/// Value of node
+	/// </summary>
+	string _value;
+
+	/// <summary>
+	/// Flag for color of node
+	/// </summary>
+	bool IsBlack;
+
+
+public:
+
 };
 
